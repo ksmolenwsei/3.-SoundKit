4f704341aec1cbf3843618d52ce9fd09876baf64
ksmolenwsei <krystian.smolen@microsoft.wsei.edu.pl>
2019-02-07 16:10:02 +0100

    add event for keypress, play sound on different pressed key

--- JavaScript/skrypt.js
+++ JavaScript/skrypt.js
@@ -1,3 +1,42 @@
-window.addEventListener('keypress', (e) => {
-    console.log(e.code)
+const sounds = {
+    KeyZ: "boom",
+    KeyX: "clap",
+    KeyC: "hihat",
+    KeyV: "openhat",
+    KeyB: "snare",
+    KeyN: "tink",
+    KeyM: "tom"
+}
+let activeChannels = [
+    {
+      name: 'channel1',
+      sounds: [],
+      isRecording: false,
+      recStart: null
+    }, 
+    {
+      name: 'channel2',
+      sounds: [],
+      isRecording: false,
+      recStart: null
+    },
+    {
+      name: 'channel3',
+      sounds: [],
+      isRecording: false,
+      recStart: null
+    },
+    {
+      name: 'channel4',
+      sounds: [],
+      isRecording: false,
+      recStart: null
+    },
+]
+document.addEventListener("keypress", (e)=>{
+  const soundName = sounds[e.code];
+    let audioDOM = document.querySelector(`#${soundName}`);
+    audioDOM.currentTime = 0;
+    audioDOM.play();
 })
+ 
